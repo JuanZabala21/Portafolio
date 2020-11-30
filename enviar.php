5c28a3c645ac2ed0ef7f911afde6cbed3f389cdc
Juan <juanpablozabala21@gmail.com>
2020-11-29 22:22:09 -0400

    Modificando

--- enviar.php
+++ enviar.php
@@ -1,22 +1,43 @@
-<?php 
+<?php
+ 
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $concerned_department = "";
+    $visitor_message = "";
+     
+    if(isset($_POST['name'])) {
+      $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+    }
+     
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+    }
+     
+    if(isset($_POST['subject'])) {
+        $email_title = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    }
+          
+    if(isset($_POST['message'])) {
+        $visitor_message = htmlspecialchars($_POST['message']);
+    }
+     
+    $recipient = "juanpablozabala21@gmail.com";
 
-$name = $_POST['name']; 
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message']; 
-
-$header = 'De:' .$mail. "\r\n"; 
-$header = "Content-Type: text/pain";
-
-$message = "Este mensaje ha sido enviado por: " .$name. "\r\n"; 
-$message =  "Su email es" .$email. "\r\n"; 
-$message = "Mensaje" .$_POST['message']. "\r\n"; 
-
-$para = 'juanpablozabala21@gmail.com'; 
-$subject = 'Asunto del mensaje'; 
-
-mail($para, $asunto, utf8_decode($message), $header); 
-
-header("Location:index.html");
-
-?>  
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+     
+    if(mail($recipient, $email_title, $visitor_message, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+     
+} else {
+    echo '<p>Something went wrong</p>';
+}
+ 
+?>
